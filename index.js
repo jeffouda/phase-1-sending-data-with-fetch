1c38b64df862b953bdb95406798036443aaf0f6a
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-09-26 10:33:48 +0300

    test complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  };
+
+  // Return the fetch chain so tests can detect it
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((object) => {
+      // Append the new id to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = object.id;
+      body.appendChild(p);
+    })
+    .catch((error) => {
+      // Append the error message to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      body.appendChild(p);
+    });
+}
